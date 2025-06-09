5d4eb42071569f74125fe45d640cea515bb847fa
RobertCcj <robert0963938708@gmail.com>
2025-06-09 14:42:20 +0800

    CreatePlaylist page 文字調整

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
 
 function CreatePlaylist() {
   return (
@@ -8,6 +8,13 @@ function CreatePlaylist() {
       <div className="container mx-auto p-8">
         <h1 className="text-2xl font-bold mb-4">創建個人歌單</h1>
         {/* Spotify integration will be added in the next iteration */}
+
+        {/* 敬請期待 */}
+        <div className="flex justify-center items-center h-64">
+          <p className="text-center text-base md:text-xl text-primary-600 font-medium">
+            🎧 敬請期待
+          </p>
+        </div>
       </div>
     </div>
   );
